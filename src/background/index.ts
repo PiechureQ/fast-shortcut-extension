3e56ff09bd5e792859a3d17fcc972a73a01f4f4f
Michał Piechura <piechureq@wp.pl>
2020-02-09 22:10:26 +0100

    working 'r' shortcut for reddit

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,26 @@
-//console.log('Background.js file loaded');
+browser.omnibox.setDefaultSuggestion({
+  description: "Use your fast shortcuts by typing them, little dog"
+});
 
-browser.runtime.onMessage.addListener(function (message) {
-  console.log(message);
+browser.omnibox.onInputChanged.addListener((text, addSuggestions) => {
+  console.log(text);
+});
+
+browser.omnibox.onInputEntered.addListener((text, disposition) => {
+  console.log(text);
+  const url = "https://reddit.com";
+  if (text === 'r') {
+    switch (disposition) {
+      case "currentTab":
+        browser.tabs.update({url});
+        break;
+      case "newForegroundTab":
+        browser.tabs.create({url});
+        break;
+      case "newBackgroundTab":
+        browser.tabs.create({url, active: false});
+        break;
+    }
+  }
 });
 
